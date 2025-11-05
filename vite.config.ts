17b27add6b4dcf4ef181156950e31c96423ae5be
jonathanlie <jonathanlie94@gmail.com>
2025-11-06 01:11:04 +0800

    Install tailwind, fix vite test config

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
-import { defineConfig } from 'vite';
+import { defineConfig } from 'vitest/config';
 import react from '@vitejs/plugin-react';
 import path from 'path';
+import tailwindcss from '@tailwindcss/vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
@@ -35,4 +36,3 @@ export default defineConfig({
     },
   },
 });
-

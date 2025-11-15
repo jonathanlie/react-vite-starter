d79a8cb0ab85c2e8d72444ea98af292a33c82980
jonathanlie <jonathanlie94@gmail.com>
2025-11-16 01:27:13 +0800

    Install libraries required for portfolio site: - MDX vite plugin to lazy load MDX content - react-flow for graph - fuse.js for fuzzy search

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,12 @@
 import { defineConfig } from 'vitest/config';
 import react from '@vitejs/plugin-react';
+import mdx from '@mdx-js/rollup';
 import path from 'path';
 import tailwindcss from '@tailwindcss/vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [react(), mdx(), tailwindcss()],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
